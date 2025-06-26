9e8f03451b767c17b9bc298e45513eeeaa465d72
airoyjones0429 <airoyjones0429@gmail.com>
2025-06-26 13:53:27 +0800

    想讓手機點畫面也可以用的JS

---
+++ cellplay.js
@@ -0,0 +1,20 @@
+import * as playJS from './play.js' ;
+
+//為手機執行時，增加可以操作的事件
+document.querySelectorAll('div[class="key"]').forEach(
+    key => {
+        key.addEventListener(
+            'click' , 
+            event => {
+                console.log( event.target.id ) ;
+                const keyName = event.target.id ;
+
+                //因為 id 設定時，我都用大寫，所以就不用再 .toUpperCase() 了
+                playJS.checkTypeInput( keyName , playJS.currentLetter ) ;
+
+                ///將按鈕設定為綠底白字
+                playJS.highlightKey(event.target.id ,500,"green","white");
+            }
+        )
+    }
+);
